2ee7fcc4f56edfd55bd026d568aafdf6d523b413
fantomitechno <simon@renoux.dev>
2024-06-29 15:57:25 +0200

    ✨ Timers

--- astro.config.ts
+++ astro.config.ts
@@ -4,11 +4,18 @@ import node from "@astrojs/node";
 import image from './build/image.js';
 
 export default defineConfig({
-  site: "https://short.renoux.dev",
+  site: "https://ftb.fantomitechno.dev",
   output: "server",
   adapter: node({
     mode: "standalone"
   }),
   integrations: [image()],
-  scopedStyleStrategy: "where"
+  scopedStyleStrategy: "where",
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
 });
